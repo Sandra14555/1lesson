e498f19605421bd2c69b14f61653fa0c18e90564
Sandra14555 <132147169+Sandra14555@users.noreply.github.com>
2023-05-14 22:35:48 +0300

    sims

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import random
+
+
 # class Human:
 #     def __init__(self, name = "Human"):
 #         self.name = name
@@ -25,10 +27,10 @@ import random
 # car.add_passenger(vasya)
 # car.add_passenger(petya)
 # car.print_passengers_names()
-#______________SIMS____________________________
+# ______________SIMS____________________________
 class Human:
-    def __init__(self, name = "Human", job = None, home = None, car=None):
-        self.name= name
+    def __init__(self, name="Human", job=None, home=None, car=None):
+        self.name = name
         self.money = 100
         self.gladness = 50
         self.satiety = 50
@@ -51,18 +53,94 @@ class Human:
         self.job = Job(job_list)
 
     def eat(self):
-        if self.home.food <=0:
+        if self.home.food <= 0:
             self.shopping("food")
         else:
-            if self.satiety >=100:
+            if self.satiety >= 100:
                 self.satiety = 100
                 return
             self.satiety += 5
             self.home.food -= 5
 
+    def work(self):
+        if self.car.drive():
+            pass
+        else:
+            if self.car.fuel < 20:
+                self.shopping("fuel")
+                return
+        self.money += self.job.salary
+        self.gladness -= self.job.gladness_less
+        self.satiety -= 4
+
+    def shopping(self, manage):
+        if self.car.drive():
+            pass
+        else:
+            if self.car.fuel < 20:
+                manage = "fuel"
+            else:
+                self.to_repair()
+                return
+        if manage == "fuel":
+            print("I bought fuel")
+            self.money -= 100
+            self.car.fuel += 100
+        elif manage == "food":
+            print("I bought food")
+            self.money -= 50
+            self.home.food += 50
+        elif manage == "delicacies":
+            print("Yay!!! Delicious")
+            self.money -= 15
+            self.gladness += 10
+            self.satiety += 2
+    def chill(self):
+        self.gladness += 10
+        self.home.mess += 5
+
+    def clean_home(self):
+        self.gladness -= 10
+        self.home.mess += 0
+
+    def to_repair(self):
+        self.car.strength += 100
+        self.money -= 50
+
+    def days_indexes(self, day):
+        day = f"Today {day} of {self.name}'s life"
+        print(f"{day:^50}","\n")
+        human_indexes = self.name + "'s indexes"
+        print(f"{human_indexes}^50","\n")
+        print(f"Money - {self.money}")
+        print(f"Satiety - {self.satiety}")
+        print(f"Gladness - {self.gladness}")
+        home_indexes = "Home indexes"
+        print(f"{home_indexes:^50}","\n")
+        print(f"Food - {self.home.food}")
+        print(f"Mess - {self.home.mess}")
+        car_indexes = f"{self.car.brand} car indexes"
+        print(f"{car_indexes:^50}","\n")
+        print(f"Fuel - {self.car.fuel}")
+        print(f"Strength - {self.car.strength}")
+    def is_alive(self):
+        if self.gladness< 0:
+            print("Depression...")
+            return False
+        if self.satiety < 0:
+            print("Dead...")
+            return False
+        if self.money <= 500:
+            print("Bankrupt...")
+            return False
+        
 class Auto:
     pass
+
+
 class House:
     pass
+
+
 class Job:
     pass
