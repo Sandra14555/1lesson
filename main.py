50517f3566eaf998917ebb3452b55133d3d87ff8
Sandra14555 <132147169+Sandra14555@users.noreply.github.com>
2023-05-18 22:55:43 +0300

    :(

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ class Student:
 
     def to_study(self):
         print('Time to study')
-        self.progress += 0.13
+        self.progress += 0.7
         self.gladness -= 5
 
     def to_sleep(self):
@@ -42,34 +42,46 @@ class Student:
         self.money -= 10
 
     def to_work(self):
-        if self.money <= 20:
-            print('You have to work')
+        print('I have to work')
+        self.money += 5
+        self.progress += 0.3
+        self.gladness -= 0.5
+
+    def to_solve_problems(self):
+        if self.money <= 10:
+            print('I need to work')
             self.money += 5
-            self.progress += 0.5
-            self.gladness -= 0.5
+            self.progress += 0.3
+            self.gladness -= 0.1
+        elif self.progress < 1:
+            print('I have to study')
+            self.progress += 0.7
+            self.gladness -= 0.1
+
 
 
     def is_alive(self):
         if self.progress < -0.5:
             print('Cast out...')
             self.alive = False
-        elif self.gladness <= 0:
+        elif self.gladness < 0:
             print('Hello Depression...')
             self.alive = False
-        elif self.progress > 5:
+        elif self.progress > 10:
             print('passed externally')
             self.alive = False
-        elif self.money <= 10:
+        elif self.money < 5:
             print('You ran out of money...')
             self.alive = False
 
     def end_of_day(self):
         print(f'Gladness = {self.gladness}')
         print(f'Progress = {self.progress, 2}')
+        print(f'Money = {self.money}')
     def live(self, day):
         day = "Day" + str(day) +" of " + self.name + " life. "
         print(f"{day:=^50}")
-        live_cube = random.randint(1, 4)
+        live_cube = random.randint(1, 5)
         if live_cube == 1:
             self.to_study()
         elif live_cube == 2:
@@ -78,6 +90,8 @@ class Student:
             self.to_chill()
         elif live_cube == 4:
             self.to_work()
+        elif live_cube == 5:
+            self.to_solve_problems()
         self.end_of_day()
         self.is_alive()
 nick = Student(name = "Nick")
