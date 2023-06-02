a6f480e5732373bbcc41e3ceb00dee1d21170ac8
Sandra14555 <132147169+Sandra14555@users.noreply.github.com>
2023-06-02 18:32:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,30 @@
-print('test')
-print('Hello world!')
+class Dog:
+    def __init__(self, name="dog"):
+        self.name = name
+
+
+class Owner:
+    def __init__(self, name):
+        self.name = name
+        self.dogs = []
+
+    def add_dog(self, dog):
+        self.dogs.append(dog)
+
+    def print_dogs_names(self):
+        if self.dogs:
+            print(f"Names of {self.name}'s dogs:")
+            for dog in self.dogs:
+                print(dog.name)
+        else:
+            print(f"There are no dogs by {self.name}")
+
+
+
+mars = Dog("Mars")
+petya = Owner("Petya")
+
+petya.add_dog(mars)
+
+petya.print_dogs_names()
+
