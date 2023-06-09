ac8c9f52dca964c5718f15e5872c2c31f77bae96
Sandra14555 <132147169+Sandra14555@users.noreply.github.com>
2023-06-09 19:51:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,17 +13,17 @@ import requests
 # response = requests.get("https://httpbin.org/get")
 # print(response.text)
 
-response = requests.get("https://coinmarketcap.com/")
+response = requests.get("https://bank.gov.ua/ua/markets/exchangerate-chart?cn%5B%5D=USD")
 response_text = response.text
 
 coin_list = []
 
 response_parse = response_text.split("<span>")
 for parse_elem1 in response_parse:
-    if parse_elem1.startswith("$"):
+    if parse_elem1.startswith("₴"):
         for parse_elem2 in parse_elem1.split("</span>"):
-            if parse_elem2.startswith("$") and parse_elem2[1].isdigit():
+            if parse_elem2.startswith("₴") and parse_elem2[1].isdigit():
                 coin_list.append(parse_elem2)
 
-btc = coin_list[0]
-print("BTC =", btc)
+usd = coin_list[0]
+print("Usd =", usd)
