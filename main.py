5dc5e449af03a95b03c157adfa108f0c2699c575
Sandra14555 <132147169+Sandra14555@users.noreply.github.com>
2023-06-10 18:56:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,40 @@
-print('test')
-print('Hello world!')
+class Calculator:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+
+    def addition(self):
+        return self.num1 + self.num2
+
+    def subtraction(self):
+        return self.num1 - self.num2
+
+    def multiplication(self):
+        return self.num1 * self.num2
+
+    def division(self):
+        if self.num2 != 0:
+            return self.num1 / self.num2
+        else:
+            return "Error: Division by zero is not allowed."
+
+
+
+num1 = int(input("Input first number: "))
+num2 = int(input("Input second number: "))
+
+
+calculator = Calculator(num1, num2)
+
+
+result_addition = calculator.addition()
+result_subtraction = calculator.subtraction()
+result_multiplication = calculator.multiplication()
+result_division = calculator.division()
+
+
+print("Addition:", result_addition)
+print("Subtraction:", result_subtraction)
+print("Multiplication:", result_multiplication)
+print("Division:", result_division)
+
